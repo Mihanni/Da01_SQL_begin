f33f4fe13d0d1d0d376abe2214ea2f569422394c
Mihanni <104977982+Mihanni@users.noreply.github.com>
2023-12-27 00:37:38 +0700

    Update and rename practice_01.sql to Buoi3_Practice.sql

---
+++ Buoi3_Practice.sql
@@ -0,0 +1,9 @@
+//ex1: hackerank-revising-the-select-query.
+SELECT NAME FROM CITY WHERE COUNTRYCODE="USA" AND POPULATION >120000;
+//done
+//ex2: hackerank-japanese-cities-attributes.
+SELECT * FROM CITY
+WHERE COUNTRYCODE="JPN"
+//ex3: hackerank-weather-observation-station-1.
+SELECT CITY,STATE FROM STATION 
+ex4: hackerank-weather-observation-station-6.
